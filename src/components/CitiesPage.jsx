4c9eb451308f5c0f794824326d0da1be83718cd7
LuisFelipeZ <luisfelpzamorano@gmail.com>
2024-11-05 09:28:53 -0500

    solucion error y los que faltan t.t

--- src/components/CitiesPage.jsx
+++ src/components/CitiesPage.jsx
@@ -26,6 +26,9 @@ const CitiesPage = () => {
     if (status === 'loading') return <p>Loading cities...</p>;
     if (status === 'failed') return <p>Error: {error}</p>;
 
+    // Verifica el tipo de filteredCities
+    console.log("filteredCities:", filteredCities); // Depuración
+
     return (
         <div className="container mx-auto">
             <h2 className="text-4xl font-bold text-center mb-10">Popular Mytineraries</h2>
@@ -36,11 +39,11 @@ const CitiesPage = () => {
                 onChange={handleSearchChange}
                 className="w-full mb-6 p-2 border border-gray-300 rounded-md"
             />
-            {filteredCities.length === 0 ? (
+            {Array.isArray(filteredCities) && filteredCities.length === 0 ? (
                 <p className="text-center text-gray-600">No cities match your search.</p>
             ) : (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mb-12">
-                    {filteredCities.map((city) => (
+                    {Array.isArray(filteredCities) && filteredCities.map((city) => (
                         <div key={city._id} className="shadow-lg rounded-lg overflow-hidden bg-white border-4 border-blue-500 hover:scale-105 hover:shadow-2xl transition-transform duration-300 ease-in-out">
                             <img src={city.photo} alt={city.name} className="w-full h-64 object-cover" />
                             <div className="p-6">
