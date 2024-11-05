d0598a0ea7d491b92c6de6ffe3b7c5cc9ae042b0
LuisFelipeZ <luisfelpzamorano@gmail.com>
2024-11-05 16:21:55 -0500

    store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cityReducer from './slices/citySlice';
+import itinerariesReducer from '../store/reducer/itinerariesReducer';
+
 
 const store = configureStore({
     reducer: {
         city: cityReducer,
+        itineraries: itinerariesReducer,
+
     },
 });
 
 export default store;
+
